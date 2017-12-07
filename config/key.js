b8d941aa8192366713db35b929106266d114baa6
Suha Park <suhapark@Suhas-MacBook-Pro.local>
2017-12-06 23:38:19 -0800

    added oauth

---
+++ config/key.js
@@ -0,0 +1,12 @@
+module.exports = {
+        google: {
+            clientID: '383855380712-h8iai93lvm05or36gaq6kklscvsm7696.apps.googleusercontent.com',
+            clientSecret: '_HS0IxWlFmyg8tfQFJiUx7YO'
+        },
+        mongodb: {
+            dbURI: 'mongodb://iamshaunjp:test@ds151024.mlab.com:51024/oauth-test'
+        },
+        session: {
+            cookieKey: 'groupiris'
+        }
+};
