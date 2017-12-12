e73f0afee89eb4b9b0e91ed66ee687e456ac8045
Nadir <Jaffan_Nadir@hotmail.com>
2017-12-11 21:08:38 -0800

    Nadir Sequelize

---
+++ models/todo.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  var Order = sequelize.define("Order", {
+    Customer_Address: {
+      type: DataTypes.STRING
+    },
+    Customer_Name: {
+      type: DataTypes.STRING
+    },
+    Customer_PhoneNumber: {
+      type: DataTypes.STRING
+    },
+      Pickup_Location: {
+      type: DataTypes.STRING
+    }
+  }, {
+    timestamps: false
+  });
+    return Order;
+  };
+
+  
+ 
